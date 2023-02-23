83cb7e1289ff43d13c5ff4032df5c78ddf513258
bushmango <steve@stevebushman.com>
2023-02-22 19:24:14 -0600

    basic map

--- src/game/game2.ts
+++ src/game/game2.ts
@@ -6,9 +6,11 @@ const logger = createLogger('game2')
 
 // import { Display, Scheduler, KEYS, RNG } from 'rot-js/lib/index'
 import * as ROT from 'rot-js'
+import Digger from 'rot-js/lib/map/digger'
 
 interface IGame {
   display: ROT.Display
+  map: Digger
 }
 
 let _game: IGame | null = null
@@ -18,14 +20,24 @@ export const getGame = (): IGame => {
     document.body.appendChild(display.getContainer())
 
     // SHOW(display.getContainer())
+
     display.setOptions({
-      width: 30,
+      width: 40,
+      height: 20,
       fontSize: 48,
       fontStyle: 'bold',
-      bg: '#a00',
+      bg: '#666',
     })
+
+    ROT.RNG.setSeed(1234)
+    var map = new ROT.Map.Digger(40, 20, {})
+    map.create(() => {
+      console.log('map generator finished')
+    })
+
     _game = {
       display,
+      map,
     }
   }
   return _game
@@ -66,13 +78,83 @@ const mainLoop = () => {
 
 const draw = () => {
   let game = getGame()
-  let { display } = game
+  let { display, map } = game
   display.clear()
   // display.draw(5, 4, '@', '', '')
-  display.draw(15, 4, '%', '#0f0', '') /* foreground color */
-  display.draw(25, 4, '#', '#f00', '#009') /* and background color */
+  // display.draw(15, 4, '%', '#0f0', '') /* foreground color */
+  // display.draw(25, 4, '#', '#f00', '#009') /* and background color */
+
+  //mainUi.drawAll()
+
+  // display.DEBUG()
+
+  // Draw map
+
+  var drawDoor = (x, y) => {
+    display.draw(x, y, '+', '', 'red')
+  }
+  let rooms = map.getRooms()
+
+  for (var roomIdx = 0; roomIdx < rooms.length; roomIdx++) {
+    var room = rooms[roomIdx]
+
+    let l = room.getLeft()
+    let r = room.getRight()
+    let t = room.getTop()
+    let b = room.getBottom()
+
+    for (let i = l; i <= r; i++) {
+      for (let j = t; j <= b; j++) {
+        display.draw(i, j, '', '', 'blue')
+      }
+    }
+
+    for (let i = l - 1; i < r + 1; i++) {
+      display.draw(i, t - 1, '-', '', 'yellow')
+      display.draw(i, b + 1, '-', '', 'yellow')
+    }
+
+    for (let j = t - 1; j < b + 1; j++) {
+      display.draw(l - 1, j, '|', '', 'yellow')
+      display.draw(r + 1, j, '|', '', 'yellow')
+    }
+
+    display.draw(l - 1, t - 1, 'A', '', 'yellow')
+    display.draw(r + 1, t - 1, 'B', '', 'yellow')
+    display.draw(r + 1, b + 1, 'C', '', 'yellow')
+    display.draw(l - 1, b + 1, 'D', '', 'yellow')
+
+    room.getDoors(drawDoor)
+  }
+
+  let corridors = map.getCorridors()
+  for (let corridorIdx = 0; corridorIdx < corridors.length; corridorIdx++) {
+    let c = corridors[corridorIdx]
+
+    let sx = c._startX
+    let sy = c._startY
+    let ex = c._endX
+    let ey = c._endY
+    if (sx > ex) {
+      let swap = ex
+      ex = sx
+      sx = swap
+    }
+    if (sy > ey) {
+      let swap = ey
+      ey = sy
+      sy = swap
+    }
+
+    // console.log('corridor', corridorIdx, c, sx, sy)
+
+    for (let i = sx; i <= ex; i++) {
+      for (let j = sy; j <= ey; j++) {
+        // console.log('draw', i, j)
+        display.draw(i, j, 'x', '', 'green')
+      }
+    }
+  }
 
   monsters.drawAll()
-
-  mainUi.drawAll()
 }
